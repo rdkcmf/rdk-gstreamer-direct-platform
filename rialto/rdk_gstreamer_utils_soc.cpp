d4af34acfaade9224cd0b9ccb7bb88cc362e8675
Stefan Verkoyen <sverkoyen.contractor@libertyglobal.com>
2022-06-08 15:01:20 +0200

    RDKCOM-3243 RDKCMF-8843 First rialto implementation for rdk-gstreamer-utils
    Netflix 5.3.1 RDK porting layer uses rdk-gstreamer-utils
    (gstreamer-direct-platform) for some soc specific gstreamer functions.
    When using rialto we need a rialto specific version.
    
    Reason for change: RDKCMF-8843 First rialto implementation for rdk-gstreamer-utils
    Test Procedure: None.
    Risks: Low.
    Signed-off-by: Stephen Barrett <stephen_barrett@comcast.com>
    
    (cherry picked from commit 1e4eb0d2e550157a2e913e77cfc580ff0e71445f)
    Change-Id: I0affd055c5312cf252b0e05dd02510bae8b7a6ce

---
+++ rialto/rdk_gstreamer_utils_soc.cpp
@@ -0,0 +1,202 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:
+*
+* Copyright 2022 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <stdlib.h>
+
+#include "../rdk_gstreamer_utils.h"
+namespace rdk_gstreamer_utils
+{
+    const char* getAudioDecoderName_soc()
+    {
+        const char* audiodecoder_name  = PLATFORM_AUDIODECODER;
+        return audiodecoder_name;
+    }
+
+    const char* getVideoUnderflowSignalName_soc()
+    {
+        const char* UnderflowSignalFromPlatform = PLATFORM_BUFFERUNDERFLOW_SIGNAL;
+        return UnderflowSignalFromPlatform;
+    }
+
+    const char* getAudioUnderflowSignalName_soc()
+    {
+        const char* UnderflowSignalFromPlatform = PLATFORM_BUFFERUNDERFLOW_SIGNAL;
+        return UnderflowSignalFromPlatform;
+    }
+
+    void initVirtualDisplayHeightandWidthFromPlatform_soc(unsigned int* mVirtualDisplayHeight, unsigned int* mVirtualDisplayWidth)
+    {
+        *mVirtualDisplayHeight = PLATFORM_VIRTUALDISPLAY_HEIGHT;
+        *mVirtualDisplayWidth = PLATFORM_VIRTUALDISPLAY_WIDTH;
+    }
+
+    bool IsAudioFadeSupported_soc()
+    {
+        return false;
+    }
+
+    void EaseAudio_soc(double target, uint32_t duration, rgu_Ease ease)
+    {
+        // no op
+        return;
+    }
+    void setVideoProperty_soc(GstElement *pipeline)
+    {
+        // no op. To be implemented if required later
+        return;
+    }
+
+    void processAudioGap_soc(GstElement *pipeline,gint64 gapstartpts,gint32 gapduration,gint64 gapdiscontinuity,bool audioaac)
+    {
+        // no op. To be implemented if required later
+        return;
+    }
+
+    void enableAudioSwitch_soc(GstElement *pipeline)
+    {
+        // no op. To be implemented if required later
+        return;
+    }
+
+    GstElement * configureUIAudioSink_soc(bool TTSenabled)
+    {
+        GstElement *audioSink = NULL;
+        LOG_RGU("configureUIAudioSink_soc: : connecting rialtouiaudiosink");
+        audioSink = gst_element_factory_make ("rialtouiaudiosink","rialtouiaudiosink");
+
+        return audioSink;
+    }
+
+    bool isUIAudioVGAudioMixSupported_soc()
+    {
+        return true;
+    }
+
+    unsigned getNativeAudioFlag_soc()
+    {
+        return getGstPlayFlag("native-audio");
+    }
+
+    bool isPtsOffsetAdjustmentSupported_soc()
+    {
+        return true;
+    }
+
+    int getPtsOffsetAdjustment_soc(const std::string& audioCodecString)
+    {
+        return 0;
+    }
+
+    void configAudioCap_soc(AudioAttributes *pAttrib, bool *audioaac, bool svpenabled, GstCaps **appsrcCaps)
+    {
+        gchar *caps_string;
+        LOG_RGU("Config audio codec %s sampling rate %d channel %d alignment %d",
+                pAttrib->mCodecParam.c_str(),
+                pAttrib->mSamplesPerSecond,
+                pAttrib->mNumberOfChannels,
+                pAttrib->mBlockAlignment);
+
+        if (pAttrib->mCodecParam.compare(0, 4, std::string("mp4a")) == 0)
+        {
+            LOG_RGU("####### Using AAC\n");
+            caps_string = g_strdup_printf("audio/mpeg, mpegversion=4, enable-svp=(string)%s", svpenabled ? "true" : "false");
+            *audioaac = true;
+        }
+        else
+        {
+            LOG_RGU("####### Using EAC3 \n");
+
+            caps_string = g_strdup_printf("audio/x-eac3, framed=(boolean)true, rate=(int)%u, channels=(int)%u, alignment=(string)frame, enable-svp=(string)%s",
+                                          pAttrib->mSamplesPerSecond,
+                                          pAttrib->mNumberOfChannels,
+                                          svpenabled ? "true" : "false");
+            *audioaac = false;
+        }
+        *appsrcCaps = gst_caps_from_string(caps_string);
+        g_free(caps_string);
+    }
+
+    bool performAudioTrackCodecChannelSwitch_soc(struct rdkGstreamerUtilsPlaybackGrp *pgstUtilsPlaybackGroup, const void *pSampleAttr, AudioAttributes *pAudioAttr, uint32_t *pStatus, unsigned int *pui32Delay,
+                                                 llong *pAudioChangeTargetPts, const llong *pcurrentDispPts, unsigned int *audio_change_stage, GstCaps **appsrcCaps,
+                                                 bool *audioaac, bool svpenabled, GstElement *aSrc, bool *ret)
+    {
+        return false;
+    }
+
+    void setAppSrcParams_soc(GstElement *aSrc,MediaType mediatype)
+    {
+        if (mediatype == MEDIA_VIDEO)
+            g_object_set(aSrc, "max-bytes", (guint64) 512 * 1024, NULL);
+        else
+            g_object_set(aSrc, "max-bytes", (guint64) 1 * 64 * 1024, NULL);
+    }
+
+    void setPixelAspectRatio_soc(GstCaps ** ppCaps,GstCaps *appsrcCaps,uint32_t pixelAspectRatioX,uint32_t pixelAspectRatioY)
+    {
+        return;
+    }
+
+    void deepElementAdded_soc (struct rdkGstreamerUtilsPlaybackGrp *pgstUtilsPlaybackGroup,GstBin* pipeline, GstBin* bin, GstElement* element)
+    {
+        return;
+    }
+
+    #define GST_FIFO_SIZE_MS (48)
+    void audioMixerGetDeviceInfo_soc(uint32_t& preferredFrames, uint32_t& maximumFrames)
+    {
+        uint64_t maxBytes = GST_FIFO_SIZE_MS * 48 * 4;  // 100ms of PCM data = 4800 frames * 4 bytes
+
+        maximumFrames = maxBytes / 4;
+        preferredFrames = maximumFrames / 4;
+    }
+
+    size_t audioMixerGetBufferDelay_soc(int64_t queuedBytes,int bufferDelayms)
+    {
+        return ((queuedBytes/256) * 64);
+    }
+
+    uint64_t audioMixerGetFifoSize_soc()
+    {
+        return (GST_FIFO_SIZE_MS * 48 * 4);
+    }
+
+    void switchToAudioMasterMode_soc()
+    {
+        // no op. To be implemented if required later
+        return;
+    }
+    
+    void setKeyFrameFlag_soc(GstBuffer *gstBuffer,bool val)
+    {
+        // no op. To be implemented if required later
+        return;
+    }
+
+    bool getDelayTimerEnabled_soc()
+    {
+        return true;
+    }
+
+    void SetAudioServerParam_soc(bool enabled)
+    {
+        return;
+    }
+
+} // namespace rdk_gstreamer_utils_soc.cpp
+
